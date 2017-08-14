7c2a2b238117f5dee561c6082ffa31e14c8064fb
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-14 09:52:41 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Prime
+{
+public static void main(String args[])throws IOException
+{
+int num,i,count=0;
+ Scanner scan = new Scanner(System.in);
+	  System.out.print("Enter a Number : ");
+        num = scan.nextInt();
+		
+        for(i=2; i<num; i++)
+        {
+            if(num%i == 0)
+            {
+                count++;
+                break;
+            }
+        }
+        if(count == 0)
+        {
+            System.out.print("This is a Prime Number");
+        }
+        else
+        {
+            System.out.print("This is not a Prime Number");
+        }
+    }
+}
