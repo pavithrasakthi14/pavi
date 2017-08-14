d4a13d387fdba0cbbf5ebcc4b3bf5fdc7fe1498c
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-14 09:59:14 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Odd
+{
+public static void main(String args[])throws IOException
+{
+Scanner sc=new Scanner(System.in);
+int i;
+for(i=10;i<20;i++)
+{
+System.out.println(i);
+}
+}
+}
+
