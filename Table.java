8876ef1729f0717c4ed345bcdb1798c55f1a0e1b
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-14 10:36:30 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main(String args[])throws IOException
+{
+Scanner sc=new Scanner(System.in);
+int n;
+n=sc.nextInt();
+for(i=1;i<50;i++)
+{
+Sysetm.out.println((n+"*"+i+"="+n*i);
+}
+}
+}
+
+
