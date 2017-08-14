a843be103d6b949225e814da58102c5ebeb167ff
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-14 10:01:16 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Odd
+{
+public static void main(String args[])throws IOException
+{
+Scanner sc=new Scanner(System.in);
+int i;
+for(i=10;i<20;i++)
+{
+if(i%2==0)
+{
+System.out.println(i);
+}
+}
+}
+}
